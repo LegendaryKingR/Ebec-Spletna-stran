6dc397a3c92669732b3e96943bd9c0e22277ddfb
LegendaryKingR <55704604+LegendaryKingR@users.noreply.github.com>
2022-03-22 17:19:01 +0100

    shrek

---
+++ Test/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+    $("#owl").owlCarousel({
+   
+        autoPlay: 3000, // AutoPlay  3 seconds
+   
+        items : 4,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+   
+    });
+   
+  });
